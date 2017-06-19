55d3682e86dc609ea6672afb999a728bfda69e60
zackcreach <zackcreach@gmail.com>
2017-06-18 22:12:48 -0400

    Added seconds timer.

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,9 +1,10 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = (props) => {
   return (
     <div>
-      <h1>Countdown Component!</h1>
+      <Clock totalSeconds={129}/>
     </div>
   );
 };
